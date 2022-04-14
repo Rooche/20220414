00ae3fe9f4f311f9d5b51e1dddb2e803e6a15c9c
admin <admin@YD2-21>
2022-04-14 17:27:02 +0900

    20220414

--- P20220414/src/P20220414/BoardList.java
+++ P20220414/src/P20220414/BoardList.java
@@ -9,42 +9,67 @@ public class BoardList {
 	}
 
 	// 배열에 한건 입력
-	public void addBoard(Board board) { // 클래스도 받아올수있음
+	public int addBoard(Board board) {
+		//더이상 저장공간 없을때..false
+//		boolean check = false;
+		int errorCase = -1; // 저장공간없음
+	//	boolean check = false;
 		for (int i = 0; i < boards.length; i++) {
+			if(boards[i] != null && boards[i].getBoardNo() == board.getBoardNo()) {
+				errorCase = 1; //동일한 값이 있을경우의 에러
+				break;
+			}
 			if (boards[i] == null) {
 				boards[i] = board;
+				errorCase = 0; //정상처리
 				break;
 			}
 		}
+		return errorCase;
 	}
+	// 배열에 한건 입력
+//	public void addBoard(Board board) { // 클래스도 받아올수있음
+//		for (int i = 0; i < boards.length; i++) {
+//			if (boards[i] == null) {
+//				boards[i] = board;
+//				break;
+//			}
+//		}
+//	}
 
 	// 수정 : 게시글번호로 한건을 찾아와서 내용, 제목만 바꾸도록 하겠습니다
-	public void modifyBoard(Board board) {
+	public boolean modifyBoard(Board board) {
+		boolean check = false;
 		for (int i = 0; i < boards.length; i++) {
 			if (boards[i].getBoardNo() == board.getBoardNo()) { // 지금 바꾸려고하는 보드넘버가 같은지를 비교한다
 				boards[i].setContent(board.getContent());
 				boards[i].setTitle(board.getTitle());
+				check = true;
 				break;
 			}
 		}
+		return check;
 	}
 
 	// 삭제 : 게시글번호만 있으면 삭제되게
-	public void removeBoard(int bNo) {
+	public boolean removeBoard(int bNo) {
+		boolean check = false;
 		for (int i = 0; i < boards.length; i++) {
 			if (boards[i].getBoardNo() == bNo) {
 				boards[i] = null; // 문자(숫자)가 들어간 배열에 null값을 넣어 글을 삭제함
+				check = true;
 				break;
 			}
 		}
+		return check;
 	}
-
+	//한건 조회 : 게시글번호
 	public Board searchBoard(int bNo) {
 		for (int i = 0; i < boards.length; i++) {
 			if (boards[i].getBoardNo() == bNo) {
 				int currentCnt = boards[i].getSearchCnt();
-				boards[i].setSearchCnt(currentCnt + 1);
-				return boards[i];
+				boards[i].setSearchCnt(currentCnt + 1); //조회수 증가
+				return boards[i]; //배열의 위치에 null대입
 			}
 		}
 		return null;
--- P20220414/src/P20220414/Class4BoardEx.java
+++ P20220414/src/P20220414/Class4BoardEx.java
@@ -19,16 +19,24 @@ public class Class4BoardEx {
 				System.out.print("글 번호 입력 >>");
 				int bNo = sc.nextInt();
 				sc.nextLine();
-		//		int bNo = Interger.pareInt(sc.nextLine());
+				// int bNo = Interger.pareInt(sc.nextLine());
 				System.out.println("제목 입력 >>");
 				String bTitle = sc.nextLine();
 				System.out.println("내용 입력 >>");
 				String bContent = sc.nextLine();
 				System.out.print("작성자 입력 >>");
 				String bWriter = sc.nextLine();
-				Board newBod = new Board(bNo, bTitle, bContent, bWriter); //생성자호출
-				boardList.addBoard(newBod);
-				
+				Board newBod = new Board(bNo, bTitle, bContent, bWriter); // 생성자호출
+				// boardList.addBoard(newBod);
+				int chk = boardList.addBoard(newBod);
+				if (chk == 0) {
+					System.out.println("정상입력되었습니다.");
+				} else if(chk == -1){
+					System.out.println("입력을 못했습니다");
+				} else if(chk == 1) {
+					System.out.println("이미 있는 번호입니다.");
+				}
+
 			} else if (menu == 2) {
 				System.out.println("수정할 글번호 입력>>>");
 				int bNo = Integer.parseInt(sc.nextLine());
@@ -37,34 +45,48 @@ public class Class4BoardEx {
 				System.out.println("변경할 내용 입력>>>");
 				String content = sc.nextLine();
 				Board cBoard = new Board(bNo, title, content, null);
-				
-				boardList.modifyBoard(cBoard);
+
+				if (boardList.modifyBoard(cBoard)) {
+					System.out.println("정상수정완료");
+				} else {
+					System.out.println("변경을 못했습니다");
+				}
+
 			} else if (menu == 3) {
 				Board[] boards = boardList.boardList();
 				System.out.println("게시글번호         제목                   내용      사용자    조회수");
 				System.out.println("=============================================================");
-				for(Board board : boards) {
-					if(board != null){
+				for (Board board : boards) {
+					if (board != null) {
 						board.getInfo();
 					}
 				}
-				
-			}else if (menu == 4) {
+
+			} else if (menu == 4) {
 				System.out.println("삭제할 글번호 >>");
 				int bNo = Integer.parseInt(sc.nextLine());
-				
-				boardList.removeBoard(bNo);
-				
-			}else if (menu == 5) {
+
+				if (boardList.removeBoard(bNo)) {
+					System.out.println("삭제가 되었습니다");
+				} else {
+					System.out.println("삭제가 안됐습니다");
+				}
+
+			} else if (menu == 5) {
 				System.out.println("조회할 글번호 >>");
 				int bNo = Integer.parseInt(sc.nextLine());
-				
+
 				Board getBoard = boardList.searchBoard(bNo);
-				getBoard.getDetailInfo(); // 출력을 하겠습니다.
-				
-			}else if (menu == 9) {
+				if (getBoard == null) {
+					System.out.println("조회결과가 없습니다");
+				} else {
+					getBoard.getDetailInfo(); // 출력을 하겠습니다.
+				}
+
+			} else if (menu == 9) {
 				System.out.println("프로그램을 종료합니다.");
 				break;
+
 			} else {
 				System.out.println("정확한 메뉴를 선택하세요.");
 			}
